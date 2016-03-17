be56446ec64e372a9710c0b9c04516675884643f
zgt <2506526350@qq.com>
2016-03-17 15:36:36 +0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,16 @@ User.create(email:'2506526350@qq.com', password: '12345678')
 
 puts "-"*50
 puts "创建测试管理员数据......"
-Admin.create(email:'admin@qq.com', password: '12345678')
+admin = Admin.new(email:'admin@qq.com', password: '12345678')
+
+puts "-"*50
+puts "创建测试文章数据......"
+
+(1..10).each do |article_seq|
+  admin.articles.build(
+    title: "文章标题_#{article_seq}",
+    content: "文章内容_#{article_seq}",
+    article_type: "it"
+  )
+end
+admin.save
